2c6f376b2b99aa3e5fdeb8f06ec0e064f71f114b
berniet1 <berniet1@my.erau.edu>
2015-03-04 15:58:21 -0500

    Basic items added, non-functional panel, JFrame is functional.

--- SmartHouse/src/Login.java
+++ SmartHouse/src/Login.java
@@ -1,14 +1,44 @@
 
 
+import javax.swing.*;
 
-/**
- * @author Abraham
- * @version 1.0
- * @created 26-Feb-2015 6:23:21 PM
- */
-public class Login {
+
+public class Login extends JPanel {
 
 	public Login(){
+	JPanel mainPanel = new JPanel(new SpringLayout());
+	JButton loginB = new JButton("Login");
+	JLabel userName = new JLabel("Username");
+	JLabel userPass = new JLabel("Password");
+	JButton register = new JButton("Register");
+	JTextField userField = new JTextField(20);
+	JPasswordField passField = new JPasswordField(20);
+	
+
+	
+	userName.setBounds(10, 10, 80, 25);
+	mainPanel.add(userField);
+	
+	userField.setBounds(100, 10, 160, 25);
+	mainPanel.add(userField);
+	
+
+	userPass.setBounds(10, 40, 80, 25);
+	mainPanel.add(userPass);
+
+	
+	passField.setBounds(100, 40, 160, 25);
+	mainPanel.add(passField);
+
+
+	loginB.setBounds(10, 80, 80, 25);
+	mainPanel.add(loginB);
+	
+	register.setBounds(180, 80, 80, 25);
+	mainPanel.add(register);
+	
+	
+	
 
 	}
 
@@ -31,7 +61,16 @@ public class Login {
 
 	}
 
-	public void selectionPage()(){
+	public void selectionPage(){
 
+	}
+	
+	public static void main(String[] args){
+		JFrame frame = new JFrame();
+		Login newLog = new Login();
+		frame.add(newLog);
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		
 	}
 }//end Login
