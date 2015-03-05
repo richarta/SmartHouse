18aa14e72d95e5536be07b1770c01d3b29197b9d
richarta <richarta@my.erau.edu>
2015-03-05 18:45:23 -0500

    Added to Personalize Class.

--- SmartHouse/src/Personalize.java
+++ SmartHouse/src/Personalize.java
@@ -7,9 +7,10 @@
  */
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class Personalize extends JPanel{
+public class Personalize extends JPanel {
 
 	public House m_House;
 	public Login m_Login;
@@ -29,8 +30,51 @@ public class Personalize extends JPanel{
 
 	}
 
-	public void addHouse(){
-
+	public static void addHouse(){
+		
+		//Make addHouse GUI with House tab and pane
+		JFrame framePersonalize = new JFrame("Personalize");
+		JTabbedPane panePersonalize = new JTabbedPane();
+		JPanel panelHouse = new JPanel();
+		
+		//Create button, textfields and labels
+		JButton buttonAddFloor = new JButton("Add Floor");
+		JButton buttonSaveHouse = new JButton("Save");
+		JButton buttonChange = new JButton("Change House Name");
+		JLabel labelHouseName = new JLabel("House Name:");
+		JTextField textfieldHouseName = new JTextField(20);
+		
+		//Set layout for panel
+		panelHouse.setLayout(new FlowLayout());
+		
+		//Set bounds and add buttons textfields and labels to pane
+		panelHouse.add(labelHouseName);
+		panelHouse.add(textfieldHouseName);
+		panelHouse.add(buttonSaveHouse);
+				
+		//Add tab with house panel
+		panePersonalize.addTab("House", panelHouse);
+		
+		//Add tab pane to frame
+		framePersonalize.add(panePersonalize);
+		
+		//Set size, location, and visibility of pane
+		framePersonalize.pack();
+		framePersonalize.setLocationRelativeTo(null);
+		framePersonalize.setVisible(true);
+		
+		//Listener for save button
+		buttonSaveHouse.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				
+				//Adjust House pane 
+				
+				//Create Panel for the Floor tab and set it as the index
+				JPanel panelFloor = new JPanel();
+				panePersonalize.addTab("Floors", panelFloor);
+				panePersonalize.setSelectedIndex(1);
+			}
+		});
 	}
 
 	public void remove(){
@@ -40,31 +84,46 @@ public class Personalize extends JPanel{
 	public static void main(String[] args){
 		
 		//Make username and password frame and pane
-		JFrame framePersonalize = new JFrame("Username and Password");
-		JPanel panelPersonalize = new JPanel();
+		JFrame frameUNPW = new JFrame("Username and Password");
+		JPanel panelUNPW = new JPanel();
 		
 		//Create button, textfields, and labels
 		JButton buttonSave = new JButton("Save");
-		JLabel labelUsername = new JLabel("Username");
-		JLabel labelPassword = new JLabel("Password");
+		JLabel labelUsername = new JLabel("Username:");
+		JLabel labelPassword = new JLabel("Password:");
 		JTextField textfieldUsername = new JTextField(20);
 		JTextField textfieldPassword = new JTextField(20);
 		
 		//Set layout for pane
-		panelPersonalize.setLayout(new FlowLayout());
+		panelUNPW.setLayout(new FlowLayout());
 		
 		//Add labels, textfields, and button to pane in flow order
-		panelPersonalize.add(labelUsername);
-		panelPersonalize.add(textfieldUsername);
-		panelPersonalize.add(labelPassword);
-		panelPersonalize.add(textfieldPassword);
-		panelPersonalize.add(buttonSave);
+		panelUNPW.add(labelUsername);
+		panelUNPW.add(textfieldUsername);
+		panelUNPW.add(labelPassword);
+		panelUNPW.add(textfieldPassword);
+		panelUNPW.add(buttonSave);
 		
 		//Add pane to frame
-		framePersonalize.add(panelPersonalize);
+		frameUNPW.add(panelUNPW);
 		
-		//Set size of pane and make visible
-		framePersonalize.setSize(300, 175);
-		framePersonalize.setVisible(true);
+		//Set size of size, location, and visibility of frameUNPW
+		frameUNPW.setSize(300, 175);
+		frameUNPW.setLocationRelativeTo(null);
+		frameUNPW.setVisible(true);
+		
+		//Create listener for save button
+		buttonSave.addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				
+				//Move to new GUI
+				addHouse();
+				
+				//Hide username and password GUI
+				frameUNPW.setVisible(false);
+				
+				//Save username and password in datase
+			}
+		});
 	}
 }//end Personalize
