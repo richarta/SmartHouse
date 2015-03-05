c0d22541e97cfecdcab069600ef8c0cef8c10418
shane-strutt <strutts@my.erau.edu>
2015-03-05 18:52:36 -0500

    Commiting Nothing

--- SmartHouse/src/Login.java
+++ SmartHouse/src/Login.java
@@ -1,17 +1,19 @@
 
-
 import javax.swing.*;
 
 
 public class Login extends JPanel {
 
 	public Login(){
-	JPanel mainPanel = new JPanel(new SpringLayout());
+	SpringLayout sl_mainPanel = new SpringLayout();
+	JPanel mainPanel = new JPanel(sl_mainPanel);
 	JButton loginB = new JButton("Login");
 	JLabel userName = new JLabel("Username");
 	JLabel userPass = new JLabel("Password");
 	JButton register = new JButton("Register");
 	JTextField userField = new JTextField(20);
+	sl_mainPanel.putConstraint(SpringLayout.NORTH, userField, 42, SpringLayout.SOUTH, register);
+	sl_mainPanel.putConstraint(SpringLayout.WEST, userField, 67, SpringLayout.WEST, mainPanel);
 	JPasswordField passField = new JPasswordField(20);
 	
 
@@ -68,7 +70,7 @@ public class Login extends JPanel {
 	public static void main(String[] args){
 		JFrame frame = new JFrame();
 		Login newLog = new Login();
-		frame.add(newLog);
+		frame.getContentPane().add(newLog);
 		frame.setSize(800, 600);
 		frame.setVisible(true);
 		
