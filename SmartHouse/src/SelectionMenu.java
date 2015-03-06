210f656d170c8b86b2a2d5b27bd8cdd5a461c0a6
shane-strutt <strutts@my.erau.edu>
2015-03-06 10:59:54 -0500

    Added buttons to SelectionMenu

--- SmartHouse/src/SelectionMenu.java
+++ SmartHouse/src/SelectionMenu.java
@@ -1,28 +1,52 @@
-	import javax.swing.JFrame;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.BorderLayout;
 
-	public class SelectionMenu {
+	public class SelectionMenu{
 		
-		public void finalize() throws Throwable {
-
-		}
-		public Sel.Ent.Button(){
-
-		}
-
-		public Sel.env.Button(){
-
-		}
-
-		public Sel.Sec.Button(){
+		public SelectionMenu() {
+			
+			JFrame frame;
+			JButton envrbtn,secbtn,entbtn;
+			JPanel panel;
+			
+			panel = new JPanel();
+			
+			envrbtn = new JButton("Environmental");
+			envrbtn.setBounds(50,50,100,50);
+			secbtn = new JButton("Security");
+			secbtn.setBounds(200,200,200,200);
+			entbtn = new JButton("Entertainment");
+			entbtn.setBounds(300,300,300,300);
+			
+			panel.add(envrbtn);
+			panel.add(secbtn);
+			panel.add(entbtn);
+			panel.setVisible(true);
+			
+			frame = new JFrame("Main Menu");
+			frame.setSize(800, 600);
 			
 		}
+		
+	//	public void finalize() throws Throwable {
+
+	//	}
+	//	public Sel.Ent.Button(){
+
+		//}
+
+		//public Sel.env.Button(){
+
+		//}
+
+		//public Sel.Sec.Button(){
+			
+		//}
 		
 		public static void main(String[] args){
 			
-			JFrame frame = new JFrame();
-			frame.setSize(800, 600);
-			frame.setVisible(true);
+			new SelectionMenu();
+
 		}
 
 	}
