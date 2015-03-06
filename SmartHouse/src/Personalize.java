143fb1d2ae49be5897bc7f7d156cd73f043ccfa8
richarta <richarta@my.erau.edu>
2015-03-05 20:59:23 -0500

    Changes to Personalize class.

--- SmartHouse/src/Personalize.java
+++ SmartHouse/src/Personalize.java
@@ -27,22 +27,47 @@ public class Personalize extends JPanel {
 		JLabel labelPassword = new JLabel("Password:");
 		JTextField textfieldUsername = new JTextField(20);
 		JTextField textfieldPassword = new JTextField(20);
-		
-		//Set layout for pane
-		panelUNPW.setLayout(new FlowLayout());
+		SpringLayout sl_panelUNPW = new SpringLayout();
+		sl_panelUNPW.putConstraint(SpringLayout.SOUTH, buttonSave, 93, SpringLayout.NORTH, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.EAST, buttonSave, 265, SpringLayout.WEST, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.WEST, textfieldUsername, 69, SpringLayout.WEST, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.EAST, textfieldUsername, -10, SpringLayout.EAST, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.EAST, labelUsername, -6, SpringLayout.WEST, textfieldUsername);
+		sl_panelUNPW.putConstraint(SpringLayout.WEST, labelPassword, 6, SpringLayout.WEST, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.EAST, labelPassword, -13, SpringLayout.WEST, textfieldPassword);
+		sl_panelUNPW.putConstraint(SpringLayout.NORTH, textfieldUsername, 8, SpringLayout.NORTH, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.SOUTH, textfieldUsername, -12, SpringLayout.NORTH, textfieldPassword);
+		sl_panelUNPW.putConstraint(SpringLayout.WEST, textfieldPassword, 69, SpringLayout.WEST, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.WEST, labelUsername, 0, SpringLayout.WEST, labelPassword);
+		sl_panelUNPW.putConstraint(SpringLayout.EAST, textfieldPassword, -10, SpringLayout.EAST, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.NORTH, textfieldPassword, 38, SpringLayout.NORTH, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.NORTH, buttonSave, 68, SpringLayout.NORTH, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.WEST, buttonSave, 137, SpringLayout.WEST, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.NORTH, labelPassword, 34, SpringLayout.NORTH, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.SOUTH, labelPassword, 68, SpringLayout.NORTH, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.NORTH, labelUsername, 0, SpringLayout.NORTH, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.SOUTH, labelUsername, 34, SpringLayout.NORTH, panelUNPW);
+		panelUNPW.setLayout(sl_panelUNPW);
 		
 		//Add labels, textfields, and button to pane in flow order
 		panelUNPW.add(labelUsername);
 		panelUNPW.add(textfieldUsername);
 		panelUNPW.add(labelPassword);
 		panelUNPW.add(textfieldPassword);
+		
+		JLabel label = new JLabel("");
+		sl_panelUNPW.putConstraint(SpringLayout.NORTH, label, 68, SpringLayout.NORTH, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.WEST, label, 0, SpringLayout.WEST, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.SOUTH, label, 102, SpringLayout.NORTH, panelUNPW);
+		sl_panelUNPW.putConstraint(SpringLayout.EAST, label, 137, SpringLayout.WEST, panelUNPW);
+		panelUNPW.add(label);
 		panelUNPW.add(buttonSave);
 		
 		//Add pane to frame
 		frameUNPW.getContentPane().add(panelUNPW);
 		
 		//Set size of size, location, and visibility of frameUNPW
-		frameUNPW.setSize(287, 125);
+		frameUNPW.setSize(291, 142);
 		frameUNPW.setLocationRelativeTo(null);
 		frameUNPW.setVisible(true);
 		
