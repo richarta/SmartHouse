3d66b105510ad4c26dc137b23802038297d63401
shane-strutt <strutts@my.erau.edu>
2015-03-06 09:16:15 -0500

    Fixed SelectionMenu Page to show frame.

--- SmartHouse/src/SelectionMenu.java
+++ SmartHouse/src/SelectionMenu.java
@@ -1,40 +1,29 @@
+	import javax.swing.JFrame;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.SpringLayout;
 
-import java.awt.BorderLayout;
-
-/**
- * @author Abraham
- * @version 1.0
- * @created 26-Feb-2015 6:23:30 PM
- */
-public class SelectionMenu {
-
-	public SelectionMenu(){
-
-	}
-
-	public void finalize() throws Throwable {
-
-	}
-	public Sel.Ent.Button(){
-
-	}
-
-	public Sel.env.Button(){
-
-	}
-
-	public Sel.Sec.Button(){
+	public class SelectionMenu {
 		
-	public static void main(String[] args){
+		public void finalize() throws Throwable {
+
+		}
+		public Sel.Ent.Button(){
+
+		}
+
+		public Sel.env.Button(){
+
+		}
+
+		public Sel.Sec.Button(){
+			
+		}
 		
-		JFrame frame = new JFrame();
-		frame.setSize(800, 600);
-		frame.setVisible(true);
+		public static void main(String[] args){
+			
+			JFrame frame = new JFrame();
+			frame.setSize(800, 600);
+			frame.setVisible(true);
+		}
 
 	}
-}//end SelectionMenu
+
