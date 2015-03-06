0793d082a96e651c3561c5bfb3e28ebe2f789d18
shane-strutt <strutts@my.erau.edu>
2015-03-06 17:13:55 -0500

    Made a very basic main menu

--- SmartHouse/src/SelectionMenu.java
+++ SmartHouse/src/SelectionMenu.java
@@ -1,30 +1,52 @@
 import javax.swing.*;
 import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import net.miginfocom.swing.MigLayout;
+import java.awt.GridLayout;
+import java.awt.CardLayout;
 
-	public class SelectionMenu{
+	public class SelectionMenu extends JPanel{
 		
 		public SelectionMenu() {
 			
+
 			JFrame frame;
 			JButton envrbtn,secbtn,entbtn;
 			JPanel panel;
 			
 			panel = new JPanel();
-			
-			envrbtn = new JButton("Environmental");
-			envrbtn.setBounds(50,50,100,50);
-			secbtn = new JButton("Security");
-			secbtn.setBounds(200,200,200,200);
-			entbtn = new JButton("Entertainment");
-			entbtn.setBounds(300,300,300,300);
-			
-			panel.add(envrbtn);
-			panel.add(secbtn);
-			panel.add(entbtn);
 			panel.setVisible(true);
 			
 			frame = new JFrame("Main Menu");
 			frame.setSize(800, 600);
+			frame.getContentPane().add(panel, BorderLayout.CENTER);
+			panel.setLayout(null);
+			
+
+			entbtn = new JButton("Entertainment");
+			entbtn.setBounds(475, 211, 125, 50);
+			panel.add(entbtn);
+			
+			JButton helpbtn = new JButton("Help");
+			helpbtn.setBounds(337, 485, 100, 50);
+			panel.add(helpbtn);
+			envrbtn = new JButton("Environmental");
+			envrbtn.setBounds(175, 211, 125, 50);
+			panel.add(envrbtn);
+			
+			JButton logout = new JButton("LogOut");
+			logout.setBounds(337, 424, 100, 50);
+			panel.add(logout);
+
+			secbtn = new JButton("Security");
+			secbtn.setBounds(325, 211, 125, 50);
+			panel.add(secbtn);
+
+			frame.setVisible(true);
 			
 		}
 		
@@ -42,12 +64,12 @@ import java.awt.BorderLayout;
 		//public Sel.Sec.Button(){
 			
 		//}
-		
 		public static void main(String[] args){
-			
+
 			new SelectionMenu();
 
 		}
+
 
 	}
 
