9d6fabec7b116d229752578cacd3378a660146fe
shane-strutt <strutts@my.erau.edu>
2015-03-17 15:27:39 -0400

    Added simple Help stuff

--- SmartHouse/src/SelectionMenu.java
+++ SmartHouse/src/SelectionMenu.java
@@ -1,19 +1,22 @@
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+import java.awt.image.BufferedImage;
 import java.awt.GridLayout;
 import java.awt.CardLayout;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 	public class SelectionMenu extends JPanel{
-		
-		private static JFrame frame = new JFrame("Main Menu");
-		//Tim added comment for hands on quiz
 		
 		public SelectionMenu() {
 			
@@ -86,11 +89,48 @@ import java.awt.CardLayout;
 			helpbtn.addActionListener(new ActionListener() {
 
 				public void actionPerformed(ActionEvent e4) {
-
+					
+					help();
 				}
 			});
 			
 			frame.setVisible(true);
+			
+		}
+		
+		public static void help(){
+			
+			JPanel help = new JPanel();
+			JFrame helpf = new JFrame();
+			
+			helpf = new JFrame("Help");
+			helpf.setSize(850, 150);
+			
+			JLabel label1 = new JLabel("Hello and welcome to your SmartHouse remote! Once you have personlized your house, get started by clicking one of the Main Menu buttons.");
+			label1.setVerticalTextPosition(JLabel.TOP);
+			label1.setHorizontalTextPosition(JLabel.CENTER);
+			help.add(label1);
+			
+			
+			JLabel label3 = new JLabel("The Envronmental button will allow you to change settings that include power, lighting, utilities, and power saver mode");
+			label1.setVerticalTextPosition(JLabel.BOTTOM);
+			label1.setHorizontalTextPosition(JLabel.LEFT);	
+			help.add(label3);
+			
+			JLabel label4 = new JLabel("The Security button will allow you to change settings that include doors, windows, and lockdown mode");
+			label1.setVerticalTextPosition(JLabel.BOTTOM);
+			label1.setHorizontalTextPosition(JLabel.CENTER);
+			help.add(label4);
+			
+			JLabel label5 = new JLabel("The Entertainment button will allow you to change settings that include electronics, lighting, and power saver mode");
+			label1.setVerticalTextPosition(JLabel.BOTTOM);
+			label1.setHorizontalTextPosition(JLabel.RIGHT);
+			help.add(label5);
+			
+			helpf.getContentPane().add(help);
+			help.setVisible(true);
+			helpf.setVisible(true);
+			
 			
 		}
 		
