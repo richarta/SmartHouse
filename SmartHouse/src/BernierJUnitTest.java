bc06b2b243915b13ae86e8e5171ba07d7bc04d41
berniet1 <berniet1@my.erau.edu>
2015-04-20 12:51:47 -0400

    Added properly named JUnit test case.

---
+++ SmartHouse/src/BernierJUnitTest.java
@@ -0,0 +1,62 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+ /**
+ * @author Timothy Bernier
+ *
+ */
+public class BernierJUnitTest {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		User.setUsername("test");
+		User.setPassword("abc123");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCorrectLogin() throws IOException{
+		try{
+			assertEquals(true, User.checkLogin("test", "abc123"));
+		}
+		catch(IOException e){
+			fail("Login failed due to no file");
+		}
+	}
+	@Test
+	public void testIncorrectLogin() throws IOException{
+		try{
+			assertEquals(false, User.checkLogin("false", "abc123"));
+		}
+		//Catch fileNotFoundException to confirm failed login
+		catch(IOException e){
+		}
+	}
+	@Test
+	public void testIncorrectPassword() throws IOException{
+		try{
+			assertEquals(false, User.checkLogin("test", "123abc"));
+		}
+		catch(IOException e){
+			fail("Login failed due to no file");
+		}
+	}
+
+}
